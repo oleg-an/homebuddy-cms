1fc30e6e280356daadfedb5c96dace01cab267f8
Oleg An <oleg.an@sirenltd.com>
2024-04-06 23:05:48 +0700

    .

--- src/features/edit-wizard/ui/EditStep/EditStepModal.tsx
+++ src/features/edit-wizard/ui/EditStep/EditStepModal.tsx
@@ -180,53 +180,51 @@ export function EditStepModal({ step, onEdit, title }: EditStepModalProps) {
               </div>
             </div>
             <Divider />
-            <>
-              {!!fields.length && (
-                <div className="pb-5">
-                  <div className="mb-5 flex items-center justify-between">
-                    <div className="flex items-center gap-2">
-                      <div className="text-sm font-medium">Select field name</div>
-                      <Input
-                        className="input-sm w-[150px]"
-                        title=""
-                        name="select.fieldName"
-                        isErrorMessageHidden
-                        registerOptions={getRequiredValidation()}
-                      />
-                      <div className="ml-4 text-sm font-medium">Multiselect</div>
-                      <SwitcherHookForm name="select.isMultiSelect" />
-                    </div>
-                    <div className="flex items-center gap-2">
-                      <Button
-                        size="medium"
-                        variant="outline"
-                        type="button"
-                        iconLeftName="add"
-                        onClick={addOptionHandler}
-                      >
-                        Add option
-                      </Button>
-                      <Button
-                        size="medium"
-                        variant="outline-danger"
-                        type="button"
-                        iconLeftName="delete"
-                        onClick={deleteSelectHandler}
-                      >
-                        Delete select
-                      </Button>
-                    </div>
+            {!!fields.length && (
+              <div className="pb-5">
+                <div className="mb-5 flex items-center justify-between">
+                  <div className="flex items-center gap-2">
+                    <div className="text-sm font-medium">Select field name</div>
+                    <Input
+                      className="input-sm w-[150px]"
+                      title=""
+                      name="select.fieldName"
+                      isErrorMessageHidden
+                      registerOptions={getRequiredValidation()}
+                    />
+                    <div className="ml-4 text-sm font-medium">Multiselect</div>
+                    <SwitcherHookForm name="select.isMultiSelect" />
                   </div>
-                  <Table
-                    className="!m-0"
-                    variant="table-sm"
-                    data={fields}
-                    columns={columns}
-                    rowKey={uuidv4}
-                  />
+                  <div className="flex items-center gap-2">
+                    <Button
+                      size="medium"
+                      variant="outline"
+                      type="button"
+                      iconLeftName="add"
+                      onClick={addOptionHandler}
+                    >
+                      Add option
+                    </Button>
+                    <Button
+                      size="medium"
+                      variant="outline-danger"
+                      type="button"
+                      iconLeftName="delete"
+                      onClick={deleteSelectHandler}
+                    >
+                      Delete select
+                    </Button>
+                  </div>
                 </div>
-              )}
-            </>
+                <Table
+                  className="!m-0"
+                  variant="table-sm"
+                  data={fields}
+                  columns={columns}
+                  rowKey={uuidv4}
+                />
+              </div>
+            )}
           </form>
         </SideModalBody>
         <SideModalFooter>
