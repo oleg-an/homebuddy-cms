1f2d7539fdc2c808c90ac7e40fe69a6aa1e81d7a
Oleg An <oleg.an@sirenltd.com>
2024-04-08 02:35:14 +0700

    .

--- src/entities/wizard/api/query.ts
+++ src/entities/wizard/api/query.ts
@@ -1,42 +1,39 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { api } from 'shared/lib/api';
-
-import wizardMock from './example.json';
+import type { WizardModel } from 'entities/wizard';
 
 const getWizardKey = ['getWizard'];
 const getWizardList = ['getWizardList'];
 
-/*
 export function useGetWizard({ id }: { id: string }) {
   return useQuery({
     queryKey: getWizardKey,
     queryFn: () => api.get<WizardModel>(`/rest/wizards/${id}`).then((x) => x.data),
   });
 }
- */
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/*
 export function useGetWizard({ id }: { id: string }) {
   return useQuery({
     queryKey: getWizardKey,
     queryFn: () => Promise.resolve(wizardMock),
   });
 }
+ */
 
-/*
 export function useGetWizardList() {
   return useQuery({
     queryKey: getWizardList,
     queryFn: () => api.get<WizardModel[]>(`/rest/wizards`).then((x) => x.data),
   });
 }
- */
-export function useGetWizardList() {
+
+/* export function useGetWizardList() {
   return useQuery({
     queryKey: getWizardList,
     queryFn: () => Promise.resolve([wizardMock]),
   });
-}
+} */
 
 export function useUpdateWizard(id: string) {
   return useMutation({
